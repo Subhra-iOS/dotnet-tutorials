a0c0c4c5c82430a04db7d544a7a98cde047c2770
Subhra Roy <subhra.roy84@yahoo.in>
2022-08-25 00:06:21 +0530

    commit

--- Catalog/Entities/Item.cs
+++ Catalog/Entities/Item.cs
@@ -6,7 +6,7 @@ namespace  Catalog.Entities
     public record Item
     {
         public Guid Id { get; init; }
-        public string Name { get; init; }
+        public string? Name { get; init; }
         public decimal Price { get; init; }
         public DateTimeOffset CreatedDate { get; init; }
     }
