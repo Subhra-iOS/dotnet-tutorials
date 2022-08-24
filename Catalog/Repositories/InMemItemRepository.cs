5d1d08e2b56fed2effa70a2ec0b65049e263bc94
Subhra Roy <subhra.roy84@yahoo.in>
2022-08-24 22:51:06 +0530

    add Local memory repo

---
+++ Catalog/Repositories/InMemItemRepository.cs
@@ -0,0 +1,52 @@
+
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{   
+    public class InMemItemRepository
+    {
+        private readonly List<Item> items = new()
+        {
+            new Item 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Potion",
+                Price = 50,
+                CreatedDate = DateTimeOffset.UtcNow
+            },
+            new Item 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Shield",
+                Price = 100,
+                CreatedDate = DateTimeOffset.UtcNow
+            },
+            new Item 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Sword",
+                Price = 150,
+                CreatedDate = DateTimeOffset.UtcNow
+            },
+            new Item 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Iron Armour",
+                Price = 200,
+                CreatedDate = DateTimeOffset.UtcNow
+            }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+             return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+    }
+    
+}
