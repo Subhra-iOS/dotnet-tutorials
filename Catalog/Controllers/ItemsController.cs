4d8de9be25fb4944f0cc4f6f62f7175eb4656f28
Subhra Roy <subhra.roy84@yahoo.in>
2022-08-25 16:18:40 +0530

    First GET API

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,11 +1,26 @@
-
 using Microsoft.AspNetCore.Mvc;
+using Catalog.Repositories;
+using Catalog.Entities;
 
 namespace Catalog.Controllers
 {
+    // Method GET /items 
     [ApiController]
+    [Route("items")]
     public class ItemsController: ControllerBase
     {
-        
+        private readonly InMemItemRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemRepository();
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItem()
+        {
+            return repository.GetItems();
+        }
     }
 }
